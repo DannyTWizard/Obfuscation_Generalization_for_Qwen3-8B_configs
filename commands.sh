be3a5891b02a609436cd0c981f01f4c90913a551
Cameron Tice <cwtice72@gmail.com>
2025-11-11 22:41:27 +0000

    comment

--- commands.sh
+++ commands.sh
@@ -126,65 +126,64 @@ python -m src.eval --eval_config_path configs/eval/code_selection.yaml --run_pat
 python -m src.eval --eval_config_path configs/eval/code_selection.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 2400
 
 
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 2000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 2000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 0
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 2200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 2400
-
-
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 0
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 2000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 2200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 2400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 0
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1000
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 1800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 2200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/driven-dawn-4_20251110_114239 --artifact_step 2400
 
 
 
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 0
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 2000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 2200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 2400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 0
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1000
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 1800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 2000
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 2200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/likely-capybara-5_20251110_120918 --artifact_step 2400
 
 
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 0
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1400
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1600
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1800
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 2000
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 2200
-python -m src.eval --eval_config_path configs/eval/sycophancy_fact.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 2400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 0
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1000
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 1800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 2000
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 2200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/avid-violet-8_20251110_143332 --artifact_step 2400
 
 
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 0
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1000
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1400
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1600
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 1800
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 2000
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 2200
+python -m src.eval --eval_config_path configs/eval/world_affecting_reward.yaml --run_path results/4B_sycophancy_format_0/logical-cloud-7_20251110_135103 --artifact_step 2400
+
+
+### something was failing here
