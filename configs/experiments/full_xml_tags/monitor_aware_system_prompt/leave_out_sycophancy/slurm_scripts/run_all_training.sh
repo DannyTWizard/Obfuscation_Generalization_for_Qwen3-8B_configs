41f4168d1d605bfc1a6c7ac32dd1b57ec53e16b2
Puria Radmard <pr450@cam.ac.uk>
2025-12-11 12:51:54 +0000

    ready to branch for hydra additions

--- configs/experiments/full_xml_tags/monitor_aware_system_prompt/leave_out_sycophancy/slurm_scripts/run_all_training.sh
+++ configs/experiments/full_xml_tags/monitor_aware_system_prompt/leave_out_sycophancy/slurm_scripts/run_all_training.sh
@@ -37,10 +37,15 @@ echo ""
 # echo "  Job ID: $JOB_ID"
 # SUBMITTED_JOBS+=($JOB_ID)
 
-echo "Submitting training job: monitor_informed_pen_less_stronger"
-JOB_ID=$(sbatch --parsable --job-name="train-monitor_informed_pen_less_stronger" "$TRAIN_SCRIPT" "/home/pradmard/repos/Obfuscation_Generalization/configs/experiments/full_xml_tags/monitor_aware_system_prompt/leave_out_sycophancy/train_pen_less_stronger.yaml")
+echo "Submitting training job: monitor_informed_pen_even_less_stronger"
+JOB_ID=$(sbatch --parsable --job-name="train-monitor_informed_pen_even_less_stronger" "$TRAIN_SCRIPT" "/home/pradmard/repos/Obfuscation_Generalization/configs/experiments/full_xml_tags/monitor_aware_system_prompt/leave_out_sycophancy/train_pen_even_less_stronger.yaml")
 echo "  Job ID: $JOB_ID"
 SUBMITTED_JOBS+=($JOB_ID)
+
+# echo "Submitting training job: monitor_informed_pen_less_stronger"
+# JOB_ID=$(sbatch --parsable --job-name="train-monitor_informed_pen_less_stronger" "$TRAIN_SCRIPT" "/home/pradmard/repos/Obfuscation_Generalization/configs/experiments/full_xml_tags/monitor_aware_system_prompt/leave_out_sycophancy/train_pen_less_stronger.yaml")
+# echo "  Job ID: $JOB_ID"
+# SUBMITTED_JOBS+=($JOB_ID)
 
 # echo "Submitting training job: monitor_informed_pen_stronger"
 # JOB_ID=$(sbatch --parsable --job-name="train-monitor_informed_pen_stronger" "$TRAIN_SCRIPT" "/home/pradmard/repos/Obfuscation_Generalization/configs/experiments/full_xml_tags/monitor_aware_system_prompt/leave_out_sycophancy/train_pen_stronger.yaml")
