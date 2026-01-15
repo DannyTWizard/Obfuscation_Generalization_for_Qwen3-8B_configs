a3177a365e7859531fb59b5a9585795a1e6a8a9b
Puria Radmard <pr450@cam.ac.uk>
2026-01-15 11:25:46 +0000

    seed change

--- slurm_scripts/train24/full_xml_summary_nsp_training_run_seed_24.sh
+++ slurm_scripts/train24/full_xml_summary_nsp_training_run_seed_24.sh
@@ -2,19 +2,18 @@
 
 mkdir -p slurm_logs
 
-CONFIG_FILE="slurm_scripts/train67_data50/full_xml_summary_withsp_training_run_seed_67.txt"
-NUM_JOBS=$(wc -l < "$CONFIG_FILE")
-
-# Submit the array job with common args
-sbatch --array=1-${NUM_JOBS}%4 \
-    --export=CONFIG_FILE="$CONFIG_FILE" \
+sbatch --export=CONFIG_FILE=slurm_scripts/train24/full_xml_summary_nsp_training_run_seed_24.txt \
     slurm_scripts/train_dispatch.sbatch \
     --multirun \
     'hydra.sweep.subdir=${hydra.job.num}' \
     experiment=full_xml_tags/train \
     +reward/overseer=hedged_add_info_summary \
     reward.funcs.api_overseer_summary_penalty_func.penalty_weight=-0.05 \
+    ++data.source_dataset_to_system_prompt.sycophancy_fact=null \
+    ++data.source_dataset_to_system_prompt.revealing_score=null \
+    ++data.source_dataset_to_system_prompt.world_affecting_reward=null \
+    ++data.source_dataset_to_system_prompt.code_selection=null \
     ++wandb.entity=nathanielmitrani-cfis-upc \
-    train.seed=67
+    train.seed=24
 
 echo "Array job submitted. Check with: squeue -u $USER"
--- slurm_scripts/train24/full_xml_summary_withsp_training_run_seed_24.sh
+++ slurm_scripts/train24/full_xml_summary_withsp_training_run_seed_24.sh
@@ -2,12 +2,8 @@
 
 mkdir -p slurm_logs
 
-CONFIG_FILE="slurm_scripts/train67_data50/full_xml_summary_withsp_training_run_seed_67.txt"
-NUM_JOBS=$(wc -l < "$CONFIG_FILE")
-
 # Submit the array job with common args
-sbatch --array=1-${NUM_JOBS}%4 \
-    --export=CONFIG_FILE="$CONFIG_FILE" \
+sbatch --export=CONFIG_FILE=slurm_scripts/train24/full_xml_summary_withsp_training_run_seed_24.txt \
     slurm_scripts/train_dispatch.sbatch \
     --multirun \
     'hydra.sweep.subdir=${hydra.job.num}' \
@@ -15,6 +11,6 @@ sbatch --array=1-${NUM_JOBS}%4 \
     +reward/overseer=hedged_add_info_summary \
     reward.funcs.api_overseer_summary_penalty_func.penalty_weight=-0.05 \
     ++wandb.entity=nathanielmitrani-cfis-upc \
-    train.seed=67
+    train.seed=24
 
 echo "Array job submitted. Check with: squeue -u $USER"
