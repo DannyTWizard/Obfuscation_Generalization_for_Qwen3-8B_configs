294deed3fd4680d59bb3fd2860e55856b072c343
Puria Radmard <pr450@cam.ac.uk>
2026-01-16 00:02:49 +0000

    new eval scripts

---
+++ slurm_scripts/train50/full_xml_summary_withsp_training_run_seed_50.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p slurm_logs
+
+CONFIG_FILE="slurm_scripts/train50/full_xml_summary_withsp_training_run_seed_50.txt"
+NUM_JOBS=$(wc -l < "$CONFIG_FILE")
+
+# Submit the array job with common args
+sbatch --array=1-${NUM_JOBS}%4 \
+    --export=CONFIG_FILE="$CONFIG_FILE" \
+    slurm_scripts/train_dispatch.sbatch \
+    --multirun \
+    'hydra.sweep.subdir=${hydra.job.num}' \
+    experiment=full_xml_tags/train \
+    +reward/overseer=hedged_add_info_summary \
+    reward.funcs.api_overseer_summary_penalty_func.penalty_weight=-0.05 \
+    ++wandb.entity=nathanielmitrani-cfis-upc \
+    train.seed=50
+
+echo "Array job submitted. Check with: squeue -u $USER"
